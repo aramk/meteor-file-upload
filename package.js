51e73e531f08495e7728cc50294790fac18971bc
Aram Kocharyan <akarmenia@gmail.com>
2015-04-02 12:56:05 +1100

    Version bump.

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'aramk:file-upload',
   summary: 'Simple file uploads.',
-  version: '0.2.5'
+  version: '0.2.6'
 });
 
 Package.on_use(function(api) {
@@ -11,9 +11,9 @@ Package.on_use(function(api) {
     'underscore',
     'aramk:q@1.0.1_1',
     'aramk:utility@0.6.0',
-    'cfs:standard-packages@0.5.6',
+    'cfs:standard-packages@0.5.7',
     'cfs:filesystem@0.1.2',
-    'cfs:tempstore@0.1.3',
+    'cfs:tempstore@0.1.5',
     'cfs:s3@0.1.3'
   ], ['client', 'server']);
   api.addFiles([
@@ -22,10 +22,7 @@ Package.on_use(function(api) {
   api.addFiles([
     'src/server.coffee'
   ], 'server');
-  // Files is defined lazily once an adapter is found, so isn't an explicit import to avoid creating
-  // a global variable set to undefiend.
-  // api.export(['Files'], ['client', 'server']);
-  api.imply('cfs:standard-packages@0.5.6');
+  api.imply('cfs:standard-packages@0.5.7');
   api.export([
     'FileUtils'
   ], ['client', 'server']);
