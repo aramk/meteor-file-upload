c0548b267940be14907c08e1471b4dec4a86ed8a
Aram Kocharyan <akarmenia@gmail.com>
2015-04-02 12:31:01 +1100

    Fixed version for tempstore.

--- package.js
+++ package.js
@@ -13,7 +13,7 @@ Package.on_use(function(api) {
     'aramk:utility@0.6.0',
     'cfs:standard-packages@0.5.6',
     'cfs:filesystem@0.1.2',
-    'cfs:tempstore@0.1.4',
+    'cfs:tempstore@0.1.3',
     'cfs:s3@0.1.3'
   ], ['client', 'server']);
   api.addFiles([
